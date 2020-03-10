16e65a24be8bf3138ea8ddd2dc5a3ac43f05d396
hushaobao <17854256510@163.com>
2020-03-10 14:36:29 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+pip3 install scikit-learn
+pip3 install -i  https://mirrors.ustc.edu.cn/pypi/web/simple xgboost
+pip3 install -U imbalanced-learn
+pip3 install matplotlib
+pip3 install pandas
